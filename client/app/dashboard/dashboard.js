7a28910a6e22ebfd281ebf572e2d13b55fb03437
Fangting <fangtingprahl@gmail.com>
2015-09-04 15:37:19 -0500

    client - dashboard - add getAll func in serviceProject; add init in db controller; seperate service.js into two etc

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -1,12 +1,22 @@
 (function(){
   angular.module('projectyr.dashboard', [])
-  .controller('DashboardController', DashboardController)
-  .directive('timeTracker', timeTracker);
+  .controller('DashboardController', DashboardController);
 
-  function DashboardController ($scope) {
+  function DashboardController ($scope, Project) {
     $scope.start = null;
     $scope.end = null;
     $scope.actTime = 0;
+    $scope.projects = [{project_name: "Sudokool", est_time: 100, act_time:50}];
+    $scope.skills = [{skills_name: "Javascript", act_time: 100}, {skills_name: "CSS", act_time: 20}];
+
+    $scope.init = function () {
+      Project.getAll()
+        .then(function(all) {
+          $scope.skills = all.skills// [] od obj, has skill name/time.
+          $scope.projects = all.projects;
+        })
+
+    };
 
     $scope.startClock = function () {
       $scope.start = new Date();
@@ -18,46 +28,6 @@
       $scope.actTime = (($scope.end - $scope.start)/(1000*60*60)).toFixed(2);
       $scope.start = null;
     };
-  };
-
-  function timeTracker ($interval, dateFilter) {
-    function link (scope, element, attrs) {
-      var update;
-      var start;
-      function updateTime () {
-        var diff = (new Date()).getTime() - start.getTime();
-        var s = Math.floor(diff/1000) % 60;
-        var m = Math.floor(diff/(1000 * 60)) % 60;
-        var h = Math.floor(diff/(1000 * 60 * 60)) % 60;
-
-        function addZero(i) {
-          if (i < 10) {
-              i = "0" + i;
-          }
-          return i;
-        };
-
-        element.text(h + "h : " + addZero(m) + "m : " + addZero(s) + "s");
-      };
-
-      scope.$watch(attrs.timeTracker, function(value) {
-        start = value;
-        updateTime(); 
-      });
-      
-      element.on('$destroy', function() {
-        $interval.cancel(update);
-      });
-
-      update = $interval(function() {
-        updateTime();
-      }, 1000);
-    };
-
-    return {
-      link: link
-    }
-
   };
 
 })();
