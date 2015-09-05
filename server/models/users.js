666e42b412082e512215563a3599b1cab8e0e5cd
juandsierra82 <juandsierra82@gmail.com>
2015-09-04 22:03:28 -0500

    [database] merged server to require ./db; migration is now in server folder

--- server/models/users.js
+++ server/models/users.js
@@ -1,8 +1,48 @@
 var db = require('../db.js');
+var _ = require('underscore')
 
 module.exports = {
   //fill me in
-  findUser = knex.select('username', 'users_id').from('users');
-  findPassword = knex.select('password');
+  //db all row
+  userIdQuery :  db.select('users_id').from('users'),
+  userNameQuery : db.select('username').from('users'),
+  passQuery : db.select('password').from('users'),
 
-}
+  //db specific row queries: returns value
+  findUserId: function(id){
+  	return this.userIdQuery.where(id)
+  				.then( function (rows){	
+ 						return rows[0].users_id
+  				})
+  },
+
+  findUser : function(user) {
+  	return this.userNameQuery.where(user)
+  				.then( function (rows){
+  					return rows[0].username;
+  				})
+  },
+
+  findPassword : function(pass){
+  	return this.passQuery.where(pass);
+  				.then( function (rows){
+  				return rows[0].password;
+  				})
+  }
+
+  //db check queries returns boolean
+  checkUser: function(user) {
+  	this.userIdQuery
+  		.then( function (rows){
+  			return _.contains(rows, user)
+  		})
+  	},
+
+  checkPassword: function(pass){
+  	this.passQuery
+  		.then( function (rows){
+  			return _.contains(rows, pass)
+  		})
+  }
+
+};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var db = require('./db');
 
 var app = express();
 
