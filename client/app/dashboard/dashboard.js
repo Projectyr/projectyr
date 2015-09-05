d454afe2809bbe1504d5922328d50537f9fade9c
Fangting <fangtingprahl@gmail.com>
2015-09-04 19:12:26 -0500

    client - add () under buttons in Dashboad page

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -6,7 +6,7 @@
     $scope.start = null;
     $scope.end = null;
     $scope.actTime = 0;
-    $scope.projects = [{project_name: "Sudokool", est_time: 100, act_time:50}];
+    $scope.projects = [{project_name: "Sudokool", est_time: 100, act_time:50}, {project_name: "Sudokool-Fangting", est_time: 100, act_time:50}];
     $scope.skills = [{skills_name: "Javascript", act_time: 100}, {skills_name: "CSS", act_time: 20}];
 
     $scope.init = function () {
@@ -28,6 +28,10 @@
       $scope.actTime = (($scope.end - $scope.start)/(1000*60*60)).toFixed(2);
       $scope.start = null;
     };
+
+    $scope.timeAssign = function () {
+      
+    }
   };
 
 })();
