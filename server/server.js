c56e331c3c4b0c9d556bedad31219a23c1e4cbdb
Aaron Wolfe <aar.wolfe@gmail.com>
2015-09-11 18:51:44 -0400

    [server] Prototype created for server.js

--- server/server.js
+++ server/server.js
@@ -1,60 +1,92 @@
+/* Import Statements */ 
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var db = require('./db');
+var jwt = require('jwt-simple');
+var parse = require('body-parser');
+var bcrypt = require('bcrypt-nodejs');
+var db = require('database');
+var Users = require('./models/users.js');
+var Skills = require('./models/skills.js');
+var Projects = require('./models/projects.js');
+var Utils = require('./lib/utils.js'); 
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.set('jwtTokenSecret', 'jmoney');
+var port = 3000;
 
-// fangting changed to the client side directory
-app.use(express.static(path.join(__dirname, '../client')));
-
-// fangting disabled the '/' since Angular is gonna take care of that
-//app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function(req, res){
+  res.send("<h1>Server is operational -- Projectyr</h1>");
 });
 
-// error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.post('/users/signin', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  var user = Users.findUser(req.body.username);
+  //did the user enter a valid username?
+  if(!user[0]) {
+    res.send('username does not exist');
+  } else {
+    //did the submitted password match the password stored in the database?
+    var submittedPassword = Utils.hashPassword(req.body.password);
+    if(Users.checkPassword(submittedPassword, user.password)) {
+      var token = jwt.encode(username, 'jmoney');
+      res.JSON({token: token, hasWIP: Projects.hasInProgress(username)});
+    } else {
+      res.send('invalid password');
+    }
+  }
 });
 
-app.listen('3000')
-module.exports = app;
+app.post('/users/signup', function(req, res){
+  var newUser = req.body.username;
+  var newPass = req.body.password;
+  //does the username already exist?
+  if(!Users.findUser(newUser)) {
+    res.send('username is taken, please choose another');
+  } else {
+    Utils.hashPassword(newPass)
+      .then(function(hash) {
+        Users.insertUser(newUser, hash);
+      })
+      .then(function() {
+        var token = jwt.encode(username, 'jmoney');
+        res.JSON({token: token});
+      })
+  }
+});
+
+app.post('/projects/create', function(req, res){
+  //assuming that req.body.projects = { project: { name: <projectname> ,  est: <estimated_time>, skills: { skillname: 0, skillname: 0 ... }  }
+  var username = jwt.decode(req.headers['x-access-token'], 'jmoney');
+  var project = req.body.project;
+  
+  if(Projects.duplicateProject(username, project)) {
+    res.send('Project already exists.')
+  } else {
+    Projects.insertProject(username, project);
+    res.send('success')
+  }
+});
+
+app.get('/dashboard', function(req, res) {
+  var username = jwt.decode(req.headers['x-access-token'], 'jmoney');
+  res.JSON({projects: Utils.userDashboard(username)});
+});
+
+app.post('/dashboard', function(req, res) {
+  var username = jwt.decode(req.headers['x-access-token'], 'jmoney');
+  var projects = req.body.project;
+
+  for(var project in projects) {
+    var projId = Projects.findProjectId(username, project.name);
+    for(var skill in project.skills) {
+      Skills.updateSkillTime(skill, projId, project.skills[skill]);
+    }
+  }
+  res.send('success');
+})
+
+app.listen(3000, function() {
+  console.log("Listening to localhost, port #: ", + port);
+});
