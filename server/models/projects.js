5202bf477db5731bbaf002bce9c1fe164f9fdbfd
Aaron Wolfe <aar.wolfe@gmail.com>
2015-09-11 18:40:42 -0400

    [server] Completed basic functions for project and skills model.  Added userDashboard function for utils.

--- server/models/projects.js
+++ server/models/projects.js
@@ -1,6 +1,73 @@
 var db = require('../db.js');
-var users = require('./users.js');
+var Users = require('./users.js');
+var Skills = require('./skills.js');
+var _ = require('../../node_modules/underscore/underscore.js');
 
 module.exports = {
-  //fill me in
+  getAllProjects: function(user) {
+    var userId = Users.findUserId(user);
+    return db.select()
+      .from('projects')
+      .where('users_id', '=', userId)
+      .then(function(rows){
+        return rows;
+      });
+  },
+
+  getActiveProjects = function(user) {
+    var userId = Users.findUserId(user);
+    return db.select()
+      .from('projects')
+      .where('users_id', '=', userId)
+      //we may have to change status to done, and the value might be true/false instead of active/complete
+      .andWhere('done', '=', 'false')
+      .then(function(rows){
+        return rows;
+      });
+  },
+
+  hasInProgress: function(user) {
+    var projects = this.getActiveProjects(user);
+    return projects.length > 0;
+  },
+
+  insertProject: function(user, project) {
+    //need req.body.project to be an object with the following format:  { name:, est:, skills: {skillname: 0, skillname2: 0, etc.}}
+    var userId = Users.findUserId(user);
+    db('projects').returning('projects_id').insert({project_name: project.name, est_time: project.est, users_id: userId})
+      .then(function(projId) {
+        for(var skill in project.skills) {
+          var newSkill = Skills.findSkill(skill);
+          if(!newSkill) {
+            var skillID = Skills.insertSkill(skill);
+            db('skill_times').insert({act_time: 0, users_id: userId, projects_id: projId, skills_id: skillID});
+          } else {
+            db('skill_times').insert({act_time: 0, users_id: userId, projects_id: projId, skills_id: newSkill});
+          }
+        }
+      })
+  },
+
+  duplicateProject: function(user, project) {
+    var userId = Users.findUserId(user);
+    var projects = db.select()
+      .from('projects')
+      .where('users_id', '=', userId)
+      .andWhere('project_name', '=', project.name)
+      .then(function(results) {
+        return results;
+      })
+    return projects.length > 0;
+  },
+
+  findProjectId: function(user, project) {
+    var userId = Users.findUserId(user);
+    return db.select()
+      .from('projects')
+      .where('project_name', '=', project)
+      .andWhere('users_id', '=', userId)
+      .then(function(result) {
+        result[0].projects_id;
+      })
+  }
 }
--- server/models/skills.js
+++ server/models/skills.js
@@ -1,5 +1,63 @@
 var db = require('../db.js');
+var Users = require('./users.js');
+var Projects = require('./projects.js');
 
 module.exports = {
-  //fill me in
+  getProjectSkills: function(projId) {
+    /*db('skill_times')
+        .join('skills', 'skill_times.skills_id', '=', 'skills.skills_id')
+        .select()
+        .where('projects_id', projId)
+        .then(function(rows) {
+          return rows;
+        });
+        Should return: [{skills_id, skills_name, skill_times_id, act_time, users_id, projects_id}, ...]
+     */
+    return db.select()
+      .from('skill_times')
+      .where('projects_id', '=', projId)
+      .then(function(rows) {
+        for(var row in rows) {
+          row.skill_name = this.getSkillName(row.skills_id);
+        }
+        return rows;
+      })
+  },
+
+  getSkillName: function(skillId) {
+    return db.select()
+      .from('skills')
+      .where('skills_id', '=', skillId)
+      .then(function(rows) {
+        return rows[0].skills_name;
+      })
+  },
+
+  findSkill: function(skillName) {
+    return db.select()
+      .from('skills')
+      .where('skills_name', '=', skillName)
+      .then(function(result) {
+        if(result.length) {
+          return result[0].skills_id
+        } else {
+          return null;
+        }
+      })
+  },
+
+  insertSkill: function(skillName) {
+    return db('skills').returning('skills_id').insert({skills_name: skillName})
+      .then(function(id) {
+        return id;
+      });
+  },
+
+  updateSkillTime: function(skillName, projectId, time) {
+    var skillId = this.findSkill(skillName);
+    db('skill_times').where('projectId', '=', projectId)
+      .andWhere('skills_id', '=', skillId)
+      .increment('act_time', time);
+  }
+  
 }
