e3912b9b70c058dce4965183dcab31fee2aad560
Aaron Wolfe <aar.wolfe@gmail.com>
2015-09-14 19:48:33 -0500

    [documentation] added comments to utils.js and server.js

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,26 +1,15 @@
+/* These are helper functions that don't necessarily interact with the database. */
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
-//var db = require('./db')
 
 module.exports = {
-// encryption to be called everytime a user logs in or signs up
-
-hashPassword : function(password){
+  hashPassword: function(password) {
     var cipher = Promise.promisify(bcrypt.hash);
     return cipher(password, null, null)
       .then(function(hash) {
         return hash;
       });
-  },
-
-  userDashboard: function(username) {
-    var projects = Projects.getAllProjects(username);
-    for(var project in projects) {
-      project.skills = Skills.getProjectSkills(project.projects_id)
-    }
-    return projects;
   }
-
 }
 
 
